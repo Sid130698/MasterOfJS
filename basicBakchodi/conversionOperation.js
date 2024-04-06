331c561dd45e560d77263181c6114d7bb7fbaa58
Sid130698 <sidsingh130698@gmail.com>
2024-04-07 03:55:58 +0530

    learning more conversion

--- basicBakchodi/conversionOperation.js
+++ basicBakchodi/conversionOperation.js
@@ -38,3 +38,20 @@ console.log(booleanIsLoggedIn);
 - isLoggedIn = [1, 2, 3] (array) booleanIsLoggedIn = true
 - isLoggedIn = function() { console.log("Hello!"); } (function) booleanIsLoggedIn = true
 */
+
+let someNumber = 33;
+let someNumberInString = String(someNumber);
+console.log(someNumberInString);
+
+/*
+- someNumber = 33 (number) someNumberInString = "33"
+- someNumber = 0 (number) someNumberInString = "0"
+- someNumber = true (boolean) someNumberInString = "true"
+- someNumber = false (boolean) someNumberInString = "false"
+- someNumber = null (null) someNumberInString = "null"
+- someNumber = undefined (undefined) someNumberInString = "undefined"
+- someNumber = Symbol("symbol") (symbol) someNumberInString = "Symbol(symbol)"
+- someNumber = {key: "value"} (object) someNumberInString = "[object Object]"
+- someNumber = [1, 2, 3] (array) someNumberInString = "1,2,3"
+- someNumber = function() { console.log("Hello!"); } (function) someNumberInString = "function() { console.log("Hello!"); }"
+*/
